cb59c31d4837eacb1889947c1d857294983add6c
Thiago Bustamante <tbustaman@thoughtworks.com>
2018-08-29 14:43:46 -0300

    Fix waitUntil

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const wait = ms => new Promise((r, j) => setTimeout(r, ms));
 const waitUntil = (condition, timeout, ms) => {
     const DEFAULT_INTERVAL = 50;
     const DEFAULT_TIMEOUT = 1000;
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const timeoutTimer = setTimeout(() => {
             reject(new Error('waitUntil timeout. Condition does not happened.'));
         }, timeout || DEFAULT_TIMEOUT);
